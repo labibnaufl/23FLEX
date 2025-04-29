28c6747e97940db92a031d13c8fffdfe71ef97c1
jazielaudrio <jazielaudrio.o@gmail.com>
2025-04-29 23:32:55 +0700

    Initial push: halaman rekomendasi latihan

---
+++ src/app/recommendation/page.tsx
@@ -0,0 +1,15 @@
+export default function RekomendasiPage() {
+    return (
+      <div className="p-8">
+        <h1 className="text-3xl font-bold mb-4">Rekomendasi Latihan</h1>
+        <ul className="list-disc pl-6 space-y-2">
+          <li>Push Up - 3 set 15x</li>
+          <li>Squat - 3 set 20x</li>
+          <li>Plank - tahan 60 detik</li>
+          <li>Burpees - 3 set 10x</li>
+          <li>Mountain Climbers - 3 set 30 detik</li>
+        </ul>
+      </div>
+    );
+  }
+  
